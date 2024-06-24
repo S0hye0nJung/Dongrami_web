7a0cb956e607dc6c128e03253eab29b0f809e62b
Shinwoo63 <shin_63@naver.com>
2024-06-24 18:15:43 +0900

    마이리뷰, 개인정보수정페이지

--- dongrami_without_security/src/main/java/com/lec/Impl/ReviewServiceImpl.java
+++ dongrami_without_security/src/main/java/com/lec/Impl/ReviewServiceImpl.java
@@ -1,5 +1,34 @@
 package com.lec.Impl;
 
-public class ReviewServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lec.entity.Review;
+import com.lec.repository.ReviewRepository;
+import com.lec.service.ReviewService;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class ReviewServiceImpl implements ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+    @Autowired
+    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    @Override
+    public List<Review> getAllReviews() {
+        return reviewRepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void deleteReview(int reviewId) {
+        reviewRepository.deleteById(reviewId);
+    }
 }
