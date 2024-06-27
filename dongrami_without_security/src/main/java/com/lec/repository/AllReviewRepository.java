d2cdd8b6f0fb007fcbfb57bb15779b32d3e9798e
Sohyeon <158130588+S0hye0nJung@users.noreply.github.com>
2024-06-27 17:06:54 +0900

    Delete dongrami_without_security/src/main/java/com/lec/repository/AllReviewRepository.java

--- dongrami_without_security/src/main/java/com/lec/repository/AllReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lec.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.lec.entity.AllReview;
-
-@Repository
-public interface AllReviewRepository extends JpaRepository<AllReview, Integer> {
-}
